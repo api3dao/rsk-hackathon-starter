dd8494acb02538baff3365535a81a570b99d288d
Jacob Greene <69164627+jgcogsystematictrading@users.noreply.github.com>
2021-11-04 05:36:59 -0700

    Update parameters.js

--- src/parameters.js
+++ src/parameters.js
@@ -2,6 +2,6 @@ const util = require('./util');
 
 module.exports = {
   providerId:
-    util.readFromReceipt('providerId') || '0x23722bcdd23e559d7151db284f290fadde9f3cb725859d476ef1f16ab315355e',
+    util.readFromReceipt('providerId') || '0x189989906bd5b4076005549386731dbcb69329d7b7ae4de32707a441a936ad78',
   endpointId: '0xf466b8feec41e9e50815e0c9dca4db1ff959637e564bb13fefa99e9f9f90453c',
 };
