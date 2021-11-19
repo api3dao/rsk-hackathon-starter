23fbbc9b81968578c726badf31f91199baab0816
Jacob Greene <69164627+jgcogsystematictrading@users.noreply.github.com>
2021-11-19 11:55:26 -0700

    Update obp-example-request.js

--- scripts/obp-example-request.js
+++ scripts/obp-example-request.js
@@ -8,7 +8,8 @@ const parameters = require('../src/parameters');
 async function main() {
   const path = 'banks.0.id';
   const type = 'bytes32';
-  
+  const providerId = '0xc6323485739cdf4f1073c1b21bb21a8a5c0a619ffb84dd56c4f4454af2802a40';
+  const endpointId = '0xbfd499b3bebd55fe02ddcdd5a2f1ab36ef75fb3ace1de05c878d0b53ce4a7296';
   const wallet = await evm.getWallet();
   const exampleClient = new ethers.Contract(
     util.readFromLogJson('ExampleClient address'),
@@ -20,8 +21,8 @@ async function main() {
   console.log('Making the request...');
   async function makeRequest() {
     const receipt = await exampleClient.makeRequest(
-      parameters.providerId,
-      parameters.endpointId,
+      providerId,
+      endpointId,
       util.readFromLogJson('Requester index'),
       util.readFromLogJson('Designated wallet address'),
       airnodeAbi.encode([{ name: '_path', type: 'bytes32', value: path},
